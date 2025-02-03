a8b69ef3dfe34974db3bda2140749149fee749a7
RambleTOT <rambletot@gmail.com>
2025-02-03 06:00:22 +0300

    ui login

--- app/src/main/java/ramble/sokol/hibyeapp/LoginFragment.kt
+++ app/src/main/java/ramble/sokol/hibyeapp/LoginFragment.kt
@@ -5,21 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import ramble.sokol.hibyeapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+
+    private var binding: FragmentLoginBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+    }
 
+    private fun init(){
+        val scaleDown = AnimationUtils.loadAnimation(requireActivity(), R.anim.text_click_anim)
+        val scaleUp = AnimationUtils.loadAnimation(requireActivity(), R.anim.text_click_anim_back)
+        binding!!.textButtonRegistration.setOnClickListener {
+            binding!!.textButtonRegistration.startAnimation(scaleDown)
+            binding!!.textButtonRegistration.startAnimation(scaleUp)
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            val registrationFragment = RegistrationFragment()
+            transaction.replace(R.id.layout_fragment, registrationFragment)
+            transaction.disallowAddToBackStack()
+            transaction.commit()
+        }
     }
 
 }
