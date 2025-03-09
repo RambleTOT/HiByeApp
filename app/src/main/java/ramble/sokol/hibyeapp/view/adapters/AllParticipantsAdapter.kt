32df97dbd1d5c907a1097948b207b96ac4437b2f
RambleTOT <rambletot@gmail.com>
2025-03-10 00:35:03 +0500

    add photo

--- app/src/main/java/ramble/sokol/hibyeapp/view/adapters/AllParticipantsAdapter.kt
+++ app/src/main/java/ramble/sokol/hibyeapp/view/adapters/AllParticipantsAdapter.kt
@@ -1,12 +1,20 @@
 package ramble.sokol.hibyeapp.view.adapters
 
+
+import android.graphics.drawable.Drawable // Правильный импорт Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import ramble.sokol.hibyeapp.R
 import ramble.sokol.hibyeapp.data.model.events.CreateUserResponse
 import ramble.sokol.hibyeapp.databinding.ItemParticipantAllBinding
-import ramble.sokol.hibyeapp.databinding.ItemParticipantBinding
 
 class AllParticipantsAdapter(
     private var participants: List<CreateUserResponse>,
@@ -43,7 +51,37 @@ class AllParticipantsAdapter(
         fun bind(participant: CreateUserResponse) {
             binding.participantName.text = participant.userName
             binding.participantDescription.text = participant.userInfo
+            if (!participant.photoLink.isNullOrEmpty()) {
 
+                Glide.with(binding.imageParticipant.context)
+                    .load(participant.photoLink)
+                    .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            binding.imageParticipant.setImageResource(R.drawable.icon_profile)
+                            return true
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+
+                            return false
+                        }
+                    })
+                    .into(binding.imageParticipant)
+            } else {
+                binding.imageParticipant.setImageResource(R.drawable.icon_profile)
+            }
         }
     }
 }
